d0fd310081a61ce0887e2b2a90fb55e4d79139fc
Erika Daniela Alemán Pérez <134649534+doomii18@users.noreply.github.com>
2026-05-23 12:41:50 -0600

    Add explicit endpoints without slashes to support old cached frontend

--- Config/urls.py
+++ Config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from Seguridad.Usuarios.API.UsuarioAPI import UsuarioViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/usuarios/registro', UsuarioViewSet.as_view({'post': 'register_user'})),
+    path('api/usuarios/login', UsuarioViewSet.as_view({'post': 'login_user'})),
     path('api/usuarios/', include('Seguridad.Usuarios.API.Urls')),
     path('api/categorias/', include('APPS.Categoria.API.Urls')),
     path('api/habitaciones/', include('APPS.Habitacion.API.Urls')),
